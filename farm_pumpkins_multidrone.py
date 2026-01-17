2bcc76d4e13dfb45b693881df45c680ad94e3e79
daniilgromovgit <grom0xff.mail@yandex.ru>
2026-01-17 19:21:36 +0300

    Create farm_pumpkins_multidrone.py

---
+++ farm_pumpkins_multidrone.py
@@ -0,0 +1,30 @@
+clear()
+set_world_size(max_drones() - 1)
+def farm_pumpkins_multidrone():
+	dead_map = []
+	world_size = get_world_size()
+	for _ in range(world_size):
+		x, y = get_pos_x(), get_pos_y()
+		use_item(Items.Water)
+		if get_ground_type() != Grounds.Soil:
+			till()
+		if not can_harvest():
+			plant(Entities.Pumpkin)
+			dead_map.append((x, y))
+		move(North)
+	while dead_map:
+		x, y = dead_map.pop(0)
+		move_to_position(x, y)
+		if not can_harvest():
+			plant(Entities.Pumpkin)
+			dead_map.append((x, y))
+		
+while True:
+	if num_drones() == max_drones():
+		farm_pumpkins_multidrone()
+		while num_drones() != 1:
+			pass
+		harvest()
+	else:
+		spawn_drone(farm_pumpkins_multidrone)
+		move(East)
